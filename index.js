8681592e3f0b912fa24abdbf6e44231e9d29bcd9
tr15tan01 <t.varamashvili@gmail.com>
2022-09-10 00:40:47 +0400

    added index html file

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000;
 
+// app.get('/', (req, res) => {
+//     res.status(200).send('Everything is OOOOOOOOOOOOK...')
+// })
+
 app.get('/', (req, res) => {
-    res.status(200).send('Everything is OOOOOOOOOOOOK...')
+    res.status(200).sendFile(__dirname + '/index.html')
 })
 
 app.listen(port, () => {
